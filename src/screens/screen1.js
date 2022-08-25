34d8cdd5a155ed7a49eca33100bf376eb71048bb
hamza <hamza23.js@gmail.com>
2022-08-25 13:52:17 +0500

    added drawer

---
+++ src/screens/screen1.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const screen1 = () => {
+  return (
+    <View>
+      <Text>screen1</Text>
+    </View>
+  )
+}
+
+export default screen1
+
+const styles = StyleSheet.create({})
