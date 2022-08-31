4945f4fb887fe9bc8e2e0a95091f60c5cfdeb9c2
hamza <hamza23.js@gmail.com>
2022-08-31 21:40:35 +0500

    some changes

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {useColorScheme, Alert} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
