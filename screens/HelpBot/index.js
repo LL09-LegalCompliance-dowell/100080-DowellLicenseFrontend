d6288886e90186bcb0f10913d622ca1fbaddb185
limzcode <kipkiruilims247@gmail.com>
2022-09-01 11:25:16 +0300

    Add files via upload

---
+++ screens/HelpBot/index.js
@@ -0,0 +1,22 @@
+import {View, Text, Dimensions, Animated} from 'react-native';
+import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+// import {Portal} from 'react-native-paper';
+
+import styles from './style';
+import Header from '../../components/Header';
+import colors from '../../../assets/colors/colors';
+
+const HelpBot = () => {
+  const sheetHeight = Dimensions.get('window').height;
+  const sheetWidth = Dimensions.get('window').width;
+
+  return (
+    <Portal>
+      <Animated.View style={[styles.container, {height: sheetHeight}]}>
+      </Animated.View>
+    </Portal>
+  );
+};
+
+export default HelpBot;
