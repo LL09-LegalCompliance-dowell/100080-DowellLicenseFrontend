34a973b507186806c9324bb7240b107fb48d9bea
George Kibe <georgekibewambui@gmail.com>
2022-09-30 11:28:34 +0300

    renaming bundle name

--- android/app/src/main/java/com/dowelllicenses/policies/MainActivity.java
+++ android/app/src/main/java/com/dowelllicenses/policies/MainActivity.java
@@ -1,4 +1,4 @@
-package com.frontend;
+package com.dowelllicenses.policies;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/dowelllicenses/policies/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/dowelllicenses/policies/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.frontend.newarchitecture.modules;
+package com.dowelllicenses.policies.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
