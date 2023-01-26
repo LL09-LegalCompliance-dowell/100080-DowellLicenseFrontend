372a14323da047ce0ab2d3e304883616b39e8757
George-Kibe <georgekibewambui@gmail.com>
2023-01-26 13:24:54 +0300

    check portfolio API

--- src/screens/Login/CreatePortfolio.js
+++ src/screens/Login/CreatePortfolio.js
@@ -1,6 +1,7 @@
 import {Alert, ActivityIndicator, Text, TouchableOpacity} from 'react-native';
 import React, {useRef, useEffect, useState} from 'react';
 import {WebView} from 'react-native-webview';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import styles from './style';
 
@@ -14,6 +15,8 @@ const CreatePortfolio = ({navigation, route}) => {
     try {
       if (url.startsWith("https://play.google.com/store/apps/details?id=com.legalzard.policies")) {
       getPortfolio();
+      }else{
+        console.log("Still exploring Client Admin!")
       }
     } catch (error) {
       Alert.alert('Error message', `${error}`);
@@ -26,23 +29,22 @@ const CreatePortfolio = ({navigation, route}) => {
       const response = await axios.post("https://100093.pythonanywhere.com/api/userinfo/", {session_id: session_id});
       //console.log("Portfolio Data",response.data)
       const portfolio = response.data.portfolio_info.filter((item) => item.product === "Legalzard")
-      console.log("Portfolio",portfolio)
+      const username = await AsyncStorage.getItem("username");
+      if(!portfolio.length){
+        Alert.alert("No portfolio detected. Try to refresh. Or try Click connect in the Legalzard Icon")
+        navigation.navigate("NoPortfolio", {"session_id":session_id, "username":username})
+        setLoading(false)
+      }
       const {owner_name, member_type, org_name, portfolio_name, role} = portfolio[0];
       console.log(member_type, org_name, portfolio_name, role)
       member_type && await AsyncStorage.setItem('member_type', member_type);
       org_name && await AsyncStorage.setItem('org_name', org_name);
       portfolio_name && await AsyncStorage.setItem('portfolio_name', portfolio_name);
       role && await AsyncStorage.setItem('role', role);
-      const username = await AsyncStorage.getItem("username");
 
-      if(!portfolio.length){
-        Alert.alert("No portfolio detected. Try to refresh. Or try Click connect in the Legalzard Icon")
-        navigation.navigate("NoPortfolio", {"session_id":session_id, "username":username | owner_name})
-        setLoading(false)
-      }else{
-        navigation.navigate("RootNavigator")
-        setLoading(false)
-      }
+      navigation.navigate("RootNavigator")
+      setLoading(false)
+      
 
     }catch(error){
       console.log("Fetching portfolio Error!",error);
@@ -58,23 +60,23 @@ const CreatePortfolio = ({navigation, route}) => {
       const response = await axios.post("https://100014.pythonanywhere.com/api/userinfo/", {session_id: session_id});
       //console.log("Portfolio Data",response.data)
       const portfolio = response.data.portfolio_info.filter((item) => item.product === "Legalzard")
+      const username = await AsyncStorage.getItem("username");
       console.log("Portfolio",portfolio)
+      if(!portfolio.length){
+        Alert.alert("No portfolio detected. Try to refresh. Or try Click connect in the Legalzard Icon")
+        navigation.navigate("NoPortfolio", {"session_id":session_id, "username":username})
+        setLoading(false)
+      }
       const {member_type, org_name, portfolio_name, role} = portfolio[0];
       console.log(member_type, org_name, portfolio_name, role)
       member_type && await AsyncStorage.setItem('member_type', member_type);
       org_name && await AsyncStorage.setItem('org_name', org_name);
       portfolio_name && await AsyncStorage.setItem('portfolio_name', portfolio_name);
       role && await AsyncStorage.setItem('role', role);
-      const username = await AsyncStorage.getItem("username");
-
-      if(!portfolio.length){
-        Alert.alert("No portfolio detected. Try to refresh. Or try Click connect in the Legalzard Icon")
-        //navigation.navigate("NoPortfolio", {"session_id":session_id, "username":username})
-        setLoading(false)
-      }else{
-        navigation.navigate("RootNavigator")
-        setLoading(false)
-      }
+      
+      navigation.navigate("RootNavigator")
+      setLoading(false)
+  
 
     }catch(error){
       console.log("Fetching portfolio Error!",error);
