efaf054c9a8b51b95e863fb2bfce6a5d8cf7cfaa
Hamza-js <mhamzaawanmp1@gmail.com>
2023-01-30 12:07:44 -0800

    Disclaimer for web

--- src/screens/PolicyGenerator/DisclaimerForWeb/Policy1.js
+++ src/screens/PolicyGenerator/DisclaimerForWeb/Policy1.js
@@ -4,21 +4,37 @@ import {ScrollView, View, Text, TextInput} from 'react-native';
 import styles from '../Cookies/style';
 import {ModalDatePicker} from 'react-native-material-date-picker';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import {email_validation} from '../validations';
 
-const Policy1 = () => {
-  const [date, setDate] = useState(new Date());
-  const [input2, setInput2] = useState('');
-  const [input3, setInput3] = useState('');
-  const [input4, setInput4] = useState('');
+
+const Policy1 = ({list}) => {
+  const [valid_email, setValid_email] = useState(true);
+
   return (
     <>
       <ScrollView style={styles.wrapper} showsVerticalScrollIndicator={false}>
+      <Text
+          style={
+            list[0]
+              ? styles.hide
+              : {color: 'red', textAlign: 'center', fontSize: 20}
+          }>
+          Please Check your inputs... You must fill all{' '}
+        </Text>
+        <Text
+          style={
+            valid_email
+              ? styles.hide
+              : {color: 'red', textAlign: 'center', fontSize: 20}
+          }>
+          Please Enter Valid Email{' '}
+        </Text>
         <Text style={styles.text_1}>Details:</Text>
         <View style={{paddingHorizontal: 11, paddingTop: 16}}>
           <Text style={styles.text_2}>Website Disclaimer Last updated:</Text>
           <TextInput
             style={styles.input}
-            value={date.toLocaleDateString()}
+            value={list[1].toLocaleDateString()}
             placeholder="dd/mm/yyyy"
             placeholderTextColor="gray"
           />
@@ -29,7 +45,7 @@ const Policy1 = () => {
               </View>
             }
             color="#489503"
-            onSelect={value => setDate(value)}
+            onSelect={value => list[2](value)}
             isHideOnSelect={true}
             initialDate={new Date()}
           />
@@ -37,28 +53,37 @@ const Policy1 = () => {
           <Text style={styles.text_2}>Website Name:</Text>
           <TextInput
             style={styles.input_vm}
-            value={input2}
+            value={list[3]}
             placeholder="Enter here"
             placeholderTextColor="gray"
-            onChangeText={value => setInput2(value)}
+            onChangeText={value => list[4](value)}
           />
           <Text style={styles.text_2}>Website URL:</Text>
           <TextInput
             style={styles.input_vm}
-            value={input3}
+            value={list[5]}
             placeholder="Enter here"
             placeholderTextColor="gray"
-            onChangeText={value => setInput3(value)}
+            onChangeText={value => list[6](value)}
           />
           <Text style={styles.text_2}>
             Enter your Support Email ID for contact us information:
           </Text>
           <TextInput
             style={styles.input_vm}
-            value={input4}
+            value={list[7]}
             placeholder="johndoe@gmail.com"
             placeholderTextColor="gray"
-            onChangeText={value => setInput4(value)}
+            onChangeText={value => {
+              if (value === '') {
+                setValid_email(true);
+              } else {
+                email_validation(value)
+                  ? setValid_email(true)
+                  : setValid_email(false);
+              }
+              list[8](value);
+            }}
           />
         </View>
       </ScrollView>
--- src/screens/PolicyGenerator/DisclaimerForWeb/Steps.js
+++ src/screens/PolicyGenerator/DisclaimerForWeb/Steps.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ProgressSteps, ProgressStep} from 'react-native-progress-steps';
 import {View} from 'react-native';
 import Header from '../../../components/Header';
 import Policy1 from './Policy1';
-import Policy4 from './Policy4';
+import Policy4 from '../Cookies/Policy4';
+import {empty_validation, email_validation} from '../validations';
+
+const generate_date = date => {
+  const temp = date.split('/');
+  return '20' + temp[2] + '-' + temp[0] + '-' + temp[1];
+};
+
 const Steps = () => {
+  const [empty_validationn, setempty_validation] = useState(true);
+  const [date, setDate] = useState(new Date());
+  const handle_date = state => setDate(state);
+  const [input_1, setInput_1] = useState('');
+  const handle_input_1 = state => setInput_1(state);
+  const [input_2, setInput_2] = useState('');
+  const handle_input_2 = state => setInput_2(state);
+  const [input_3, setInput_3] = useState('');
+  const handle_input_3 = state => setInput_3(state);
+  const [error_1, setError_1] = useState(false);
+  const states = [
+    empty_validationn,
+    date,
+    handle_date,
+    input_1,
+    handle_input_1,
+    input_2,
+    handle_input_2,
+    input_3,
+    handle_input_3,
+  ];
+  const inputs = [
+    input_1,
+    input_2,
+    input_3,
+  ];
+  // 4
+  const [input_1_4, setInput_1_4] = useState('');
+  const handle_input_1_4 = state => setInput_1_4(state);
+  const states_4 = [input_1_4, handle_input_1_4];
+
   const nextButton = {
     backgroundColor: '#489503',
     paddingHorizontal: 5,
@@ -29,6 +67,15 @@ const Steps = () => {
     paddingVertical: 5,
     alignSelf: 'center',
   };
+
+  const request_object = {
+    agreement_compliance_type: 'discliamer-for-website',
+    last_updated: generate_date(date.toLocaleDateString()),
+    website_name: input_1,
+    website_url: input_2,
+    website_contact_email: input_3,
+  };
+
   return (
     <>
       <Header title="Generator" />
@@ -45,20 +92,35 @@ const Steps = () => {
           nextBtnTextStyle={{color: 'white', fontSize: 18}}>
           <ProgressStep
             nextBtnStyle={nextButton}
-            nextBtnTextStyle={{color: 'white', fontSize: 18}}>
+            nextBtnTextStyle={{color: 'white', fontSize: 18}}
+            errors={error_1}
+            onNext={() => {
+              setempty_validation(empty_validation(inputs));
+              const x = empty_validation(inputs);
+              const y = email_validation(input_3);
+              setError_1(!(x & y));
+            }}>
             <View>
-              <Policy1 />
+              <Policy1 list={states} />
             </View>
           </ProgressStep>
-
           <ProgressStep
             nextBtnStyle={nextButton}
             nextBtnTextStyle={{color: 'white', fontSize: 18}}
             previousBtnTextStyle={{color: '#489503', fontSize: 18}}
             finishBtnText="Done"
-            previousBtnStyle={previousButton}>
+            previousBtnStyle={previousButton}
+            onSubmit={() => {
+              const y = email_validation(input_1_4);
+              const z = !y;
+              if (z) {
+                alert('please enter valid email');
+              } else {
+                //api
+              }
+            }}>
             <View>
-              <Policy4 />
+              <Policy4 list={states_4} object={request_object} />
             </View>
           </ProgressStep>
         </ProgressSteps>
