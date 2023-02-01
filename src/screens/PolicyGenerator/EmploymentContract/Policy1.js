af2f4baddd8dddfb5dfe9042a6e335a6107ac7db
Hamza-js <mhamzaawanmp1@gmail.com>
2023-02-01 10:26:19 -0800

    completed the Employement contract. only the URl remaining

--- src/screens/PolicyGenerator/EmploymentContract/Policy1.js
+++ src/screens/PolicyGenerator/EmploymentContract/Policy1.js
@@ -12,31 +12,31 @@ import colors from '../../../../assets/colors/colors';
 import {ModalDatePicker} from 'react-native-material-date-picker';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import RadioGroup from 'react-native-radio-buttons-group';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import {email_validation} from '../validations';
 
-const Policy1 = () => {
-  const [date, setDate] = useState(new Date());
-  const [input1, setInput1] = useState('');
-  const [input2, setInput2] = useState('');
-  const [input3, setInput3] = useState('');
-  const [input4, setInput4] = useState('');
-  const [input5, setInput5] = useState('');
-  const [input6, setInput6] = useState('');
-  const [input7, setInput7] = useState('');
-  const [input8, setInput8] = useState('');
-  const [input9, setInput9] = useState('');
-  const [input10, setInput10] = useState('');
+const Policy1 = ({list}) => {
+  const [valid_email, setValid_email] = useState(true);
+  const [valid_email1, setValid_email1] = useState(true);
 
   return (
     <>
       <ScrollView style={styles.wrapper} showsVerticalScrollIndicator={false}>
+        <Text
+          style={
+            list[0]
+              ? styles.hide
+              : {color: 'red', textAlign: 'center', fontSize: 20}
+          }>
+          Please Check your inputs... You must fill all{' '}
+        </Text>
         <Text style={styles.text_1}>Party details:</Text>
         <View style={{paddingHorizontal: 11, paddingTop: 16}}>
-          <Text style={styles.text_2}>When were the terms last updated?</Text>
           <Text style={{paddingVertical: 7}}>Clause 2.3</Text>
+          <Text style={styles.text_2}>When were the terms last updated?</Text>
+
           <TextInput
             style={styles.input}
-            value={date.toLocaleDateString()}
+            value={list[1].toLocaleDateString()}
             placeholder="dd/mm/yyyy"
             placeholderTextColor="gray"
           />
@@ -47,7 +47,7 @@ const Policy1 = () => {
               </View>
             }
             color="#489503"
-            onSelect={value => setDate(value)}
+            onSelect={value => list[2](value)}
             isHideOnSelect={true}
             initialDate={new Date()}
           />
@@ -58,28 +58,40 @@ const Policy1 = () => {
           </Text>
           <TextInput
             style={styles.input_vm}
-            value={input2}
+            value={list[3]}
             placeholder="Eg. John Smith Doe"
             placeholderTextColor="gray"
-            onChangeText={value => setInput2(value)}
+            onChangeText={value => list[4](value)}
           />
           <Text style={styles.text_2}>Website URL:</Text>
           <TextInput
             style={styles.input_vm}
-            value={input3}
+            value={list[5]}
             placeholder="Enter here"
             placeholderTextColor="gray"
-            onChangeText={value => setInput3(value)}
+            onChangeText={value => list[6](value)}
           />
           <Text style={{paddingVertical: 7}}>Clause 4.2b</Text>
           <Text style={styles.text_2}>Enter your Email ID:</Text>
           <TextInput
             style={styles.input_vm}
-            value={input4}
+            value={list[7]}
             placeholder="johndoe@gmail.com"
             placeholderTextColor="gray"
-            onChangeText={value => setInput4(value)}
+            onChangeText={value => {
+              if (value === '') {
+                setValid_email(true);
+              } else {
+                email_validation(value)
+                  ? setValid_email(true)
+                  : setValid_email(false);
+              }
+              list[8](value);
+            }}
           />
+          <Text style={valid_email ? styles.hide : styles.text_warning}>
+            Please Enter valid email
+          </Text>
 
           <Text style={styles.text_2}>Written Permission:</Text>
           <Text style={{paddingVertical: 7}}>Clause 4.4</Text>
@@ -89,11 +101,23 @@ const Policy1 = () => {
 
           <TextInput
             style={styles.input_vm}
-            value={input5}
+            value={list[9]}
             placeholder="johndoe@gmail.com"
             placeholderTextColor="gray"
-            onChangeText={value => setInput5(value)}
+            onChangeText={value => {
+              if (value === '') {
+                setValid_email1(true);
+              } else {
+                email_validation(value)
+                  ? setValid_email1(true)
+                  : setValid_email1(false);
+              }
+              list[10](value);
+            }}
           />
+          <Text style={valid_email1 ? styles.hide : styles.text_warning}>
+            Please Enter valid email
+          </Text>
         </View>
       </ScrollView>
     </>
--- src/screens/PolicyGenerator/EmploymentContract/Policy2.js
+++ src/screens/PolicyGenerator/EmploymentContract/Policy2.js
@@ -2,12 +2,21 @@ import React from 'react';
 import {useState} from 'react';
 import {ScrollView, View, Text, TextInput} from 'react-native';
 import styles from '../Cookies/style';
-
+import SelectDropdown from 'react-native-select-dropdown';
+import {number_validation} from '../validations';
 import {SelectList} from 'react-native-dropdown-select-list';
 
-const Policy2 = () => {
+const Policy2 = ({list}) => {
   const [input1, setInput1] = useState('');
   const [input3, setInput3] = useState('');
+  const cuurency = ['EUR', 'GBP', 'CAD', 'JPY', 'CHF', 'JPY'];
+  const findcurrency = cvalue => {
+    for (let index = 0; index < cuurency.length; index++) {
+      if (cuurency[index] === cvalue) {
+        return index;
+      }
+    }
+  };
 
   const currencies = [
     {key: '1', value: 'USD'},
@@ -21,16 +30,11 @@ const Policy2 = () => {
   return (
     <>
       <ScrollView style={styles.wrapper} showsVerticalScrollIndicator={false}>
+      <Text style={list[0]?styles.hide:{color:"red",textAlign:"center",fontSize:20}}>Please Check your inputs... You must fill all  </Text>
+
+        <Text style={styles.text_2}>Other Details:</Text>
         <View style={{paddingHorizontal: 11, paddingTop: 16}}>
-          <Text style={styles.text_2}>Other Details:</Text>
           <Text style={{paddingVertical: 7}}>Clause 7.2</Text>
-          <View>
-            <TextInput
-              style={styles.input_vm}
-              value={input1}
-              onChangeText={value => setInput1(value)}
-            />
-          </View>
           <Text style={styles.text_2}>Liability Limit</Text>
           <Text style={[styles.text_2, {paddingTop: 20}]}>
             Type your Amount & Choose Currency:
@@ -38,13 +42,20 @@ const Policy2 = () => {
 
           <View style={{flexDirection: 'row'}}>
             <TextInput
-              style={[styles.input_vm, {flex: 3, marginRight: 5}]}
-              value={input1}
-              placeholder="Enter Amount"
+              style={styles.input_vm_w}
+              value={list[1]}
+              placeholder="  Enter Amount"
               placeholderTextColor="gray"
-              onChangeText={value => setInput1(value)}
+              onChangeText={value => {
+                if (value === '') {
+                  list[10](true);
+                } else {
+                  number_validation(value) ? list[10](true) : list[10](false);
+                }
+                list[2](value);
+              }}
             />
-            <SelectList
+            {/* <SelectList
               style={{flex: 2}}
               setSelected={val => setInput3(val)}
               data={currencies}
@@ -57,8 +68,34 @@ const Policy2 = () => {
                 fontSize: 16,
                 fontWeight: '300',
               }}
+            /> */}
+            <SelectDropdown
+              data={cuurency}
+              onSelect={(selectedItem, index) => {
+                list[4](selectedItem);
+              }}
+              buttonStyle={{
+                backgroundColor: '#D9D9D9',
+                marginVertical: 12,
+                color: '#585858',
+                borderWidth: 1,
+                borderRadius: 15,
+                borderColor: '#C4C4C4',
+                height: 51,
+                width: '25%',
+              }}
+              buttonTextStyle={{
+                color: '#585858',
+                fontSize: 16,
+                fontWeight: '300',
+              }}
+              dropdownStyle={{borderRadius: 15}}
+              defaultValueByIndex={findcurrency(list[3])}
             />
           </View>
+          <Text style={list[9] ? styles.hide : styles.text_warning}>
+            Please Enter valid number
+          </Text>
 
           <Text style={{paddingVertical: 7}}>Clause 7.3</Text>
           <Text style={styles.text_2}>Liability Must Not Exceed</Text>
@@ -68,13 +105,20 @@ const Policy2 = () => {
 
           <View style={{flexDirection: 'row'}}>
             <TextInput
-              style={[styles.input_vm, {flex: 3, marginRight: 5}]}
-              value={input1}
-              placeholder="Enter Amount"
+              style={styles.input_vm_w}
+              value={list[5]}
+              placeholder="  Enter Amount"
               placeholderTextColor="gray"
-              onChangeText={value => setInput1(value)}
+              onChangeText={value => {
+                if (value === '') {
+                  list[12](true);
+                } else {
+                  number_validation(value) ? list[12](true) : list[12](false);
+                }
+                list[6](value);
+              }}
             />
-            <SelectList
+            {/* <SelectList
               style={{flex: 2}}
               setSelected={val => setInput3(val)}
               data={currencies}
@@ -87,8 +131,34 @@ const Policy2 = () => {
                 fontSize: 16,
                 fontWeight: '300',
               }}
+            /> */}
+            <SelectDropdown
+              data={cuurency}
+              onSelect={(selectedItem, index) => {
+                list[8](selectedItem);
+              }}
+              buttonStyle={{
+                backgroundColor: '#D9D9D9',
+                marginVertical: 12,
+                color: '#585858',
+                borderWidth: 1,
+                borderRadius: 15,
+                borderColor: '#C4C4C4',
+                height: 51,
+                width: '25%',
+              }}
+              buttonTextStyle={{
+                color: '#585858',
+                fontSize: 16,
+                fontWeight: '300',
+              }}
+              dropdownStyle={{borderRadius: 15}}
+              defaultValueByIndex={findcurrency(list[7])}
             />
           </View>
+          <Text style={list[11] ? styles.hide : styles.text_warning}>
+            Please Enter valid number
+          </Text>
         </View>
       </ScrollView>
     </>
