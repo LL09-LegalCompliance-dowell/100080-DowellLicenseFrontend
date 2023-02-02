9cd7d9d2580fda44997304180f584bb236c52cfe
omair <omairwaleed17gmail.com>
2023-02-02 15:17:24 +0200

    finsihed validations and apis in statement of work

--- src/screens/PolicyGenerator/StatementOfWork/Policy2.js
+++ src/screens/PolicyGenerator/StatementOfWork/Policy2.js
@@ -13,52 +13,37 @@ import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
 import RadioGroup from 'react-native-radio-buttons-group';
 
-import {SelectList} from 'react-native-dropdown-select-list';
+import { number_validation } from '../validations';
+import SelectDropdown from 'react-native-select-dropdown'
 
-const Policy2 = () => {
-  const [date, setDate] = useState(new Date());
+const Policy2 = ({list}) => {
+  const [valid_number,setValid_number]=useState(true)
+  const [valid_number1,setValid_number1]=useState(true)
+  const [valid_number2,setValid_number2]=useState(true)
+  const [valid_number3,setValid_number3]=useState(true)
+  const cuurency = [
+    "USD",
+    'EUR',
+    'GBP',
+    'CAD',
+    'JPY',
+    'CHF',
+    'JPY'
+]
+const findcurrency=(cvalue)=>{
+  for (let index = 0; index < cuurency.length; index++) {
+    if(cuurency[index]===cvalue){
+      return index 
+    }
+  }
+} 
 
-  const [input1, setInput1] = useState('');
-  const [input2, setInput2] = useState('');
-  const [input3, setInput3] = useState('');
-  const [input4, setInput4] = useState('');
-  const [input5, setInput5] = useState('');
-
-  const cities = [
-    {key: '1', value: 'Mumbai'},
-    {key: '2', value: 'Kolkata'},
-    {key: '3', value: 'Banglore'},
-    {key: '4', value: 'Delhi'},
-  ];
-  const states = [
-    {key: '1', value: 'Maharashtra'},
-    {key: '2', value: 'Karnataka'},
-    {key: '3', value: 'Goa'},
-    {key: '4', value: 'Gujarat'},
-  ];
-
-  const [radioButtons, setRadioButtons] = useState([
-    {
-      id: '1',
-      label: 'Individual',
-      value: 'Individual',
-      size: 18,
-      color: '#489503',
-      borderColor: '#C4C4C4',
-      selected: true,
-    },
-    {
-      id: '2',
-      label: 'Entity',
-      value: 'Entity',
-      size: 18,
-      color: '#489503',
-      borderColor: '#C4C4C4',
-    },
-  ]);
+  
   return (
     <>
       <ScrollView style={styles.wrapper} showsVerticalScrollIndicator={false}>
+      <Text style={list[28]?styles.hide:{color:"red",textAlign:"center",fontSize:20}}>Please Check your inputs... You must fill all  </Text>
+
         <Text style={styles.text_1}>Project Management:</Text>
         <View style={{marginHorizontal: 15}}>
           <Text style={[styles.text_1, {fontSize: 15}]}>
@@ -66,10 +51,10 @@ const Policy2 = () => {
           </Text>
           <TextInput
             style={styles.input_vm}
-            value={input1}
+            value={list[0]}
             placeholder="Enter here"
             placeholderTextColor="gray"
-            onChangeText={value => setInput1(value)}
+            onChangeText={value => list[1](value)}
           />
 
           <Text style={[styles.text_1, {fontSize: 15}]}>
@@ -77,7 +62,7 @@ const Policy2 = () => {
           </Text>
           <TextInput
             style={styles.input_vm}
-            value={date.toLocaleDateString()}
+            value={list[2].toLocaleDateString()}
             placeholder="dd/mm/yyyy"
             placeholderTextColor="gray"
           />
@@ -88,7 +73,7 @@ const Policy2 = () => {
               </View>
             }
             color="#489503"
-            onSelect={value => setDate(value)}
+            onSelect={value => list[3](value)}
             isHideOnSelect={true}
             initialDate={new Date()}
           />
@@ -98,7 +83,7 @@ const Policy2 = () => {
           </Text>
           <TextInput
             style={styles.input_vm}
-            value={date.toLocaleDateString()}
+            value={list[4].toLocaleDateString()}
             placeholder="dd/mm/yyyy"
             placeholderTextColor="gray"
           />
@@ -109,7 +94,7 @@ const Policy2 = () => {
               </View>
             }
             color="#489503"
-            onSelect={value => setDate(value)}
+            onSelect={value => list[5](value)}
             isHideOnSelect={true}
             initialDate={new Date()}
           />
@@ -121,66 +106,142 @@ const Policy2 = () => {
           <Text style={[styles.text_1, {fontSize: 15}]}>
             What is the minimum time required to complete this project?{' '}
           </Text>
-          <TextInput
+          
+        <TextInput
             style={styles.input_vm}
-            value={date.toLocaleDateString()}
-            placeholder="dd/mm/yyyy"
-            placeholderTextColor="gray"
+            value={list[6]}
+            placeholder="  Enter number"
+            placeholderTextColor="gray" 
+            onChangeText={(value)=>{
+              if(value===""){
+                setValid_number(true)
+              }
+              else{
+                number_validation(value)?setValid_number(true):setValid_number(false)
+              }
+              list[7](value)
+              
+          }}
+            maxLength={10}
           />
-          <ModalDatePicker
-            button={
-              <View style={styles.calendarPosition}>
-                <EvilIcons name={'calendar'} size={35} color="gray" />
-              </View>
-            }
-            color="#489503"
-            onSelect={value => setDate(value)}
-            isHideOnSelect={true}
-            initialDate={new Date()}
+        <Text  style={valid_number? styles.hide: styles.text_warning}>Please Enter valid number</Text>
+        <View style ={{position:"absolute",top:74,left:150}} >
+          <RadioGroup
+            radioButtons={list[8]}
+            onPress={(data)=>list[9](data)}
+            layout='row'            
           />
+        </View>
 
           <Text style={[styles.text_1, {fontSize: 15}]}>
             What is the value in respect of the time required?{' '}
           </Text>
+          <View style={{display:"flex",flexDirection:"row"}}>
           <TextInput
-            style={styles.input_vm}
-            value={input1}
-            placeholder="Enter Amount(₹)"
-            placeholderTextColor="gray"
-            onChangeText={value => setInput1(value)}
+              style={styles.input_vm_w}
+              value={list[10]}
+              placeholder="  Enter Amount"
+              placeholderTextColor="gray" 
+              onChangeText={(value)=>{
+                if(value===""){
+                  setValid_number1(true)
+                }
+                else{
+                  number_validation(value)?setValid_number1(true):setValid_number1(false)
+                }
+                list[11](value)
+                
+            }}
           />
+          <SelectDropdown
+              data={cuurency}
+              onSelect={(selectedItem, index) => {
+                list[13](selectedItem)
+              }}
+              buttonStyle={{backgroundColor:"#D9D9D9",marginVertical:12,color:"#585858",borderWidth: 1,borderRadius: 15,borderColor: '#C4C4C4',height: 51,width:"25%"}}
+              buttonTextStyle={{color:"#585858",fontSize:16,fontWeight:"300"}}
+              dropdownStyle={{borderRadius: 15}}
+              defaultValueByIndex={findcurrency(list[12])}
+            />
+        </View>
+        <Text  style={valid_number1 ? styles.hide: styles.text_warning}>Please Enter valid number</Text>
 
           <Text style={[styles.text_1, {fontSize: 15}]}>
             What is the billing rate?
           </Text>
+          <View style={{display:"flex",flexDirection:"row"}}>
           <TextInput
-            style={styles.input_vm}
-            value={input1}
-            placeholder="Enter Amount(₹)"
-            placeholderTextColor="gray"
-            onChangeText={value => setInput1(value)}
+              style={styles.input_vm_w}
+              value={list[14]}
+              placeholder="  Enter Amount"
+              placeholderTextColor="gray" 
+              onChangeText={(value)=>{
+                if(value===""){
+                  setValid_number2(true)
+                }
+                else{
+                  number_validation(value)?setValid_number2(true):setValid_number2(false)
+                }
+                list[15](value)
+                
+            }}
           />
+          <SelectDropdown
+              data={cuurency}
+              onSelect={(selectedItem, index) => {
+                list[17](selectedItem)
+              }}
+              buttonStyle={{backgroundColor:"#D9D9D9",marginVertical:12,color:"#585858",borderWidth: 1,borderRadius: 15,borderColor: '#C4C4C4',height: 51,width:"25%"}}
+              buttonTextStyle={{color:"#585858",fontSize:16,fontWeight:"300"}}
+              dropdownStyle={{borderRadius: 15}}
+              defaultValueByIndex={findcurrency(list[16])}
+            />
+        </View>
+        <Text  style={valid_number2 ? styles.hide: styles.text_warning}>Please Enter valid number</Text>
+          
 
           <Text style={[styles.text_1, {fontSize: 15}]}>
             What are the charges for “rush work”?{' '}
           </Text>
+          <View style={{display:"flex",flexDirection:"row"}}>
           <TextInput
-            style={styles.input_vm}
-            value={input1}
-            placeholder="Enter Amount(₹)"
-            placeholderTextColor="gray"
-            onChangeText={value => setInput1(value)}
+              style={styles.input_vm_w}
+              value={list[18]}
+              placeholder="  Enter Amount"
+              placeholderTextColor="gray" 
+              onChangeText={(value)=>{
+                if(value===""){
+                  setValid_number3(true)
+                }
+                else{
+                  number_validation(value)?setValid_number3(true):setValid_number3(false)
+                }
+                list[19](value)
+                
+            }}
           />
+          <SelectDropdown
+              data={cuurency}
+              onSelect={(selectedItem, index) => {
+                list[21](selectedItem)
+              }}
+              buttonStyle={{backgroundColor:"#D9D9D9",marginVertical:12,color:"#585858",borderWidth: 1,borderRadius: 15,borderColor: '#C4C4C4',height: 51,width:"25%"}}
+              buttonTextStyle={{color:"#585858",fontSize:16,fontWeight:"300"}}
+              dropdownStyle={{borderRadius: 15}}
+              defaultValueByIndex={findcurrency(list[20])}
+            />
+        </View>
+        <Text  style={valid_number3 ? styles.hide: styles.text_warning}>Please Enter valid number</Text>
 
           <Text style={[styles.text_1, {fontSize: 15}]}>
             Whom should the invoices be submitted to?{' '}
           </Text>
           <TextInput
             style={styles.input_vm}
-            value={input1}
+            value={list[22]}
             placeholder="Eg. John Smith Doe"
             placeholderTextColor="gray"
-            onChangeText={value => setInput1(value)}
+            onChangeText={value => list[23](value)}
           />
           <Text
             style={{
@@ -195,7 +256,48 @@ const Policy2 = () => {
             }}>
             *Include Middle Name
           </Text>
+          <Text style={[styles.text_1, {fontSize: 15}]}>
+        When should the invoices be submitted?
+          </Text>
+          <TextInput
+            style={styles.input_vm}
+            value={list[24].toLocaleDateString()}
+            placeholder="dd/mm/yyyy"
+            placeholderTextColor="gray"
+          />
+          <ModalDatePicker
+            button={
+              <View style={styles.calendarPosition}>
+                <EvilIcons name={'calendar'} size={35} color="gray" />
+              </View>
+            }
+            color="#489503"
+            onSelect={value => list[25](value)}
+            isHideOnSelect={true}
+            initialDate={new Date()}
+          />
+           <Text style={[styles.text_1, {fontSize: 15}]}>
+           When will the invoices be payable by after receipt?
+          </Text>
+          <TextInput
+            style={styles.input_vm}
+            value={list[26].toLocaleDateString()}
+            placeholder="dd/mm/yyyy"
+            placeholderTextColor="gray"
+          />
+          <ModalDatePicker
+            button={
+              <View style={styles.calendarPosition}>
+                <EvilIcons name={'calendar'} size={35} color="gray" />
+              </View>
+            }
+            color="#489503"
+            onSelect={value => list[27](value)}
+            isHideOnSelect={true}
+            initialDate={new Date()}
+          />
         </View>
+        
       </ScrollView>
     </>
   );
--- src/screens/PolicyGenerator/StatementOfWork/Steps.js
+++ src/screens/PolicyGenerator/StatementOfWork/Steps.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import {React,useState} from 'react'
 import {ProgressSteps, ProgressStep} from 'react-native-progress-steps';
 import { View } from 'react-native';
 import Header from '../../../components/Header';
@@ -6,6 +6,11 @@ import Policy1 from './Policy1';
 import Policy2 from './Policy2';
 import Policy3 from './Policy3';
 import Policy4 from '../Cookies/Policy4';
+import { empty_validation,email_validation,number_validation } from '../validations';
+const generate_date = (date)=>{
+  const temp = date.split("/")
+  return "20"+temp[2]+"-"+temp[0]+"-"+temp[1]
+ }
 const Steps = () => {
     const nextButton = {
         backgroundColor: '#489503',
@@ -31,6 +36,162 @@ const Steps = () => {
         paddingVertical: 5,
         alignSelf: 'center',
       };
+
+      ////////////////////////////////////////////////////////1
+      const [input1, setInput1] = useState('');
+      const handle_input1 = (state)=> setInput1(state);
+      const [input2, setInput2] = useState('');
+      const handle_input2 = (state)=> setInput2(state);
+      const [input3, setInput3] = useState('');
+      const handle_input3 = (state)=> setInput3(state);
+      const [date, setDate] = useState(new Date());
+      const handle_date = (state)=> setDate(state);
+      const [input4, setInput4] = useState('');
+      const handle_input4 = (state)=> setInput4(state);
+      var [isPress1, setIsPress1] = useState(false);
+      const handle_isPress1 = (state)=> setIsPress1(state);
+      var [isPress2, setIsPress2] = useState(false);
+      const handle_isPress2 = (state)=> setIsPress2(state);
+      var [isPress3, setIsPress3] = useState(false);
+      const handle_isPress3 = (state)=> setIsPress3(state);
+      var [isPress4, setIsPress4] = useState(false);
+      const handle_isPress4 = (state)=> setIsPress4(state);
+
+      const [input5, setInput5] = useState('');
+      const handle_input5 = (state)=> setInput5(state);
+      const [input6, setInput6] = useState('');
+      const handle_input6 = (state)=> setInput6(state);
+    
+      const [error_1, setError_1] = useState(false);
+      const [empty_validationn, setempty_validation] = useState(true);
+      const states= [input1,handle_input1,input2,handle_input2,input3,handle_input3,date,handle_date,input4,handle_input4,isPress1,handle_isPress1,isPress2,handle_isPress2,isPress3,handle_isPress3,isPress4,handle_isPress4,input5,handle_input5,input6,handle_input6,empty_validationn]
+      const inputs= [input1,input2,input3,input4,input5,input6]
+
+      ///////////////////////////////////////////////////////2
+      const [input1_2, setInput1_2] = useState('');
+      const handle_input1_2 = (state)=> setInput1_2(state);
+      const [date_2, setDate_2] = useState(new Date());
+      const handle_date_2 = (state)=> setDate_2(state);
+      const [date1_2, setDate1_2] = useState(new Date());
+      const handle_date1_2 = (state)=> setDate1_2(state);
+      const [input2_2, setInput2_2] = useState('');
+      const handle_input2_2 = (state)=> setInput2_2(state);
+
+      const [radioButtons, setRadioButtons] = useState([{
+        id: '1',
+        label: 'Days',
+        value: 'Days',
+        size: 12,
+        color: '#489503',
+        labelStyle:{color:"#585858"},
+        borderColor: '#C4C4C4',   
+      },{
+      id: '2',
+      label: 'Months',
+      value: 'Months',
+      size: 12,
+      color: '#489503',
+      borderColor: '#C4C4C4',
+      containerStyle:{marginLeft:5}  ,
+      labelStyle:{color:"#585858"},
+      selected: true,
+      }]);
+      const handle_radioButtons = (state)=> setRadioButtons(state);
+      const [input3_2, setInput3_2] = useState('');
+      const handle_input3_2 = (state)=> setInput3_2(state);
+      const [input4_2, setInput4_2] = useState("USD");
+      const handle_input4_2 = (state)=> setInput4_2(state);
+
+      const [input5_2, setInput5_2] = useState('');
+      const handle_input5_2 = (state)=> setInput5_2(state);
+      const [input6_2, setInput6_2] = useState("USD");
+      const handle_input6_2 = (state)=> setInput6_2(state);
+
+      const [input7_2, setInput7_2] = useState('');
+      const handle_input7_2 = (state)=> setInput7_2(state);
+      const [input8_2, setInput8_2] = useState("USD");
+      const handle_input8_2 = (state)=> setInput8_2(state);
+
+      const [input9_2, setInput9_2] = useState('');
+      const handle_input9_2 = (state)=> setInput9_2(state);
+
+      const [date2_2, setDate2_2] = useState(new Date());
+      const handle_date2_2 = (state)=> setDate2_2(state);
+      const [date3_2, setDate3_2] = useState(new Date());
+      const handle_date3_2 = (state)=> setDate3_2(state);
+      
+
+      const [error_2, setError_2] = useState(false);
+      const [empty_validationn_2, setempty_validation_2] = useState(true);
+      const states_2= [input1_2,handle_input1_2,date_2,handle_date_2,date1_2,handle_date1_2,input2_2,handle_input2_2,radioButtons,handle_radioButtons,input3_2,handle_input3_2,input4_2,handle_input4_2,input5_2,handle_input5_2,input6_2,handle_input6_2,input7_2,handle_input7_2,input8_2,handle_input8_2,input9_2,handle_input9_2,date2_2,handle_date2_2,date3_2,handle_date3_2,empty_validationn_2]
+      const inputs_2= [input1_2,input2_2,input3_2,input5_2,input7_2,input9_2]
+      
+      ///////////////////////////////////////////////////////3
+      const [input1_3, setInput1_3] = useState('');
+      const handle_input1_3 = (state)=> setInput1_3(state);
+      const [input2_3, setInput2_3] = useState('');
+      const handle_input2_3 = (state)=> setInput2_3(state);
+
+      const [error_3, setError_3] = useState(false);
+      const [empty_validationn_3, setempty_validation_3] = useState(true);
+      const states_3= [input1_3,handle_input1_3,input2_3,handle_input2_3,empty_validationn_3]
+      const inputs_3= [input1_3,input2_3]
+
+      ///////////////////////////////////////////////////////4
+      const [input_1_4, setInput_1_4] = useState("");
+      const handle_input_1_4 = (state)=> setInput_1_4(state);
+      const states_4= [input_1_4,handle_input_1_4]
+
+
+      const freelancer_access=[]
+      if (isPress1=== true){
+        freelancer_access.push("Client’s web hosting account (AWS)")
+      }
+      if (isPress2=== true){
+        freelancer_access.push("Client’s source code repository (GitHub)")
+      }
+      if (isPress3=== true){
+        freelancer_access.push("Client’s project management tool (Jira)")
+      }
+      if (isPress4=== true){
+        freelancer_access.push("Client’s time management took (Everhour)")
+      }
+      let x
+      if(radioButtons[0].selected ===true){
+      x="Days"
+      }
+      else if(radioButtons[1].selected ===true){
+        x="Months"
+      }
+    
+      const request_object=
+      {
+        agreement_compliance_type: "statement-of-work",
+        client_full_name: input1,
+        jurisdiction: input2,
+        project_name: input3,
+        effective_date: generate_date(date.toLocaleDateString()),
+        freelancers_full_name:input4,
+        freelancer_access:freelancer_access,
+        what_is_the_goal_of_this_project: input5,
+        deliverables_expected_in_this_scope_of_work: input6,
+        mode_of_communication_between_the_parties: input1_2,
+        when_will_the_freelancer_share_his_status_on_deliverables:  generate_date(date_2.toLocaleDateString()),
+        when_will_the_progress_meetings_occur:  generate_date(date1_2.toLocaleDateString()),
+        what_is_the_minimum_time_required_to_complete_this_project:  parseFloat(input2_2),
+        what_is_the_minimum_time_required_to_complete_this_project_unit: x,
+        what_is_value_in_respect_to_time_required: parseFloat(input3_2),
+        what_is_value_in_respect_to_time_required_currency:input4_2 ,
+        what_is_the_billing_rate: parseFloat(input5_2),
+        what_is_the_billing_rate_currency: input6_2,
+        what_is_the_charges_for_rush_work:  parseFloat(input7_2),
+        what_is_the_charges_for_rush_work_currency:input8_2,
+        whom_should_the_invoices_be_submitted_to: input9_2,
+        whom_should_the_invoices_be_submitted_to_department_name: "nil",
+        when_should_the_invoices_be_submitted:  generate_date(date2_2.toLocaleDateString()),
+        when_will_the_invoices_be_payable_by_after_receipt:  generate_date(date3_2.toLocaleDateString())
+    
+    }
     return (
         <>
           <Header title="Generator" />
@@ -47,29 +208,52 @@ const Steps = () => {
               nextBtnTextStyle={{color: 'white', fontSize: 18}}>
               <ProgressStep
                 nextBtnStyle={nextButton}
-                nextBtnTextStyle={{color: 'white', fontSize: 18}}>
+                nextBtnTextStyle={{color: 'white', fontSize: 18}}
+                 errors={error_1}
+                onNext={()=>{
+                  setempty_validation(empty_validation(inputs))
+                  const x=(empty_validation(inputs))
+                  setError_1(!(x) )
+                  
+                }}>
                 <View >
-                  <Policy1 />
+                  <Policy1 list={states} />
                 </View>
               </ProgressStep>
               <ProgressStep
                 nextBtnStyle={nextButton}
                 nextBtnTextStyle={{color: 'white', fontSize: 18}}
                 previousBtnTextStyle={{color: '#489503', fontSize: 18}}
-    
-                previousBtnStyle={previousButton}>
+                previousBtnStyle={previousButton}
+                errors={error_2}
+                onNext={()=>{
+                  setempty_validation_2(empty_validation(inputs_2))
+                  const x=(empty_validation(inputs_2))
+                  const y= number_validation(input2_2)
+                  const z= number_validation(input3_2)
+                  const l= number_validation(input5_2)
+                  const m= number_validation(input7_2)
+                  setError_2(!(x&y&z&l&m) )
+                  
+                }}>
                 <View >
-                  <Policy2 />
+                  <Policy2 list={states_2}/>
                 </View>
               </ProgressStep>
               <ProgressStep
                 nextBtnStyle={nextButton}
                 nextBtnTextStyle={{color: 'white', fontSize: 18}}
                 previousBtnTextStyle={{color: '#489503', fontSize: 18}}
-    
-                previousBtnStyle={previousButton}>
+                previousBtnStyle={previousButton}
+                errors={error_3}
+                onNext={()=>{
+                  setempty_validation_3(empty_validation(inputs_3))
+                  const x=(empty_validation(inputs_3))
+                  setError_3(!(x) )
+                  
+                }}>
                 <View >
-                  <Policy3 />
+                  <Policy3  list={states_3}/>
                 </View>
               </ProgressStep>
               <ProgressStep
@@ -79,7 +263,7 @@ const Steps = () => {
                 finishBtnText="Done"
                 previousBtnStyle={previousButton}>
                 <View >
-                  <Policy4 />
+                  <Policy4  list={states_4} object={request_object} />
                 </View>
               </ProgressStep>
             </ProgressSteps>
