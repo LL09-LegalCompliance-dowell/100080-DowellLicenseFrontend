431e72ebcdccb10428c331c9a01bd1490cc4c901
Hamza-js <mhamzaawanmp1@gmail.com>
2023-02-10 09:12:22 -0800

    Added the Disclaimer in Compatibility main screen

--- src/screens/LicenseCompatibility/index.js
+++ src/screens/LicenseCompatibility/index.js
@@ -51,7 +51,7 @@ const LicenseCompatibility = ({navigation}) => {
   const [comparison, setComparison] = useState([]);
 
   let firstFive = comparison.slice(0, 5);
-  if(firstFive.length < 5){
+  if (firstFive.length < 5) {
     firstFive = comparison.slice(0, comparison.length);
   }
 
@@ -516,8 +516,6 @@ const LicenseCompatibility = ({navigation}) => {
         </TouchableOpacity>
         {result ? (
           <>
-            {/* {comparison ? ( */}
-            {/* {Object.keys(licenseComaparison).length != 0 ? ( */}
             <>
               <FlatList
                 ListHeaderComponent={
@@ -658,7 +656,7 @@ const LicenseCompatibility = ({navigation}) => {
                         licenseTwo,
                         compatibiltyPercentage,
                         isCompatible,
-                        disclaimer
+                        disclaimer,
                       })
                     }
                     style={styles.readMoreContainer}>
@@ -718,9 +716,40 @@ const LicenseCompatibility = ({navigation}) => {
                 )}
               />
             </>
-            
           </>
-        ) : null}
+        ) : (
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginVertical: 60,
+              paddingHorizontal: 10,
+            }}>
+            <Text style={{fontSize: 18}}>DISCLAIMER</Text>
+            <Text
+              style={{fontSize: 15, textAlign: 'justify', paddingBottom: 85}}>
+              The information provided on this website does not, and is not
+              intended to, constitute legal advice; all information, content,
+              and materials available on this site are for general informational
+              purposes only. The materials on this website may contain
+              inaccuracies and typographical errors. While we endeavor to keep
+              the information up to date and correct, we make no representations
+              or warranties of any kind, express or implied, about the
+              completeness, accuracy, reliability, suitability, or availability
+              with respect to the website or the information, products,
+              services, or related graphics contained on the website for any
+              purpose. Any reliance on such information is therefore strictly at
+              your own risk. Legalzard App (Dowell) reserves the right, in its
+              sole discretion, to correct any error or omission in any portion
+              of the site. In no event will we be liable for any loss or damage
+              including without limitation, indirect or consequential loss or
+              damage, or any loss or damage whatsoever arising from loss of data
+              or profits arising out of, or in connection with, the use of this
+              website.
+            </Text>
+          </ScrollView>
+        )}
       </View>
     </>
   );
