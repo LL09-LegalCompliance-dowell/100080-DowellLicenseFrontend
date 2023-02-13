347793aa78d400fe8fed2c04053612f142356044
Hamza-js <mhamzaawanmp1@gmail.com>
2023-02-13 09:08:03 -0800

    Agreement complance disclamer

--- src/screens/PolicyGenerator/EmploymentContract/Policy1.js
+++ src/screens/PolicyGenerator/EmploymentContract/Policy1.js
@@ -11,7 +11,7 @@ import styles from '../Cookies/style';
 import colors from '../../../../assets/colors/colors';
 import {ModalDatePicker} from 'react-native-material-date-picker';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
-import RadioGroup from 'react-native-radio-buttons-group';
+import CountryPicker from 'react-native-country-picker-modal';
 import {email_validation, url_validation} from '../validations';
 
 const Policy1 = ({list}) => {
@@ -32,12 +32,70 @@ const Policy1 = ({list}) => {
         </Text>
         <Text style={styles.text_1}>Party details:</Text>
         <View style={{paddingHorizontal: 11, paddingTop: 16}}>
-          <Text style={{paddingVertical: 7}}>Clause 2.3</Text>
-          <Text style={styles.text_2}>When were the terms last updated?</Text>
+          <Text style={styles.text_2}>What is the name of the Company? </Text>
+          <TextInput
+            style={styles.input_vm}
+            value={list[1]}
+            placeholder="Enter here"
+            placeholderTextColor="gray"
+            onChangeText={value => list[2](value)}
+          />
+          <Text style={styles.text_2}>What is the Address of the Company?</Text>
+          <TextInput
+            style={styles.input_vm}
+            value={list[3]}
+            placeholder="Address Line 1"
+            placeholderTextColor="gray"
+            onChangeText={value => list[4](value)}
+          />
+          <TextInput
+            style={styles.input_vm}
+            value={list[5]}
+            placeholder="Address Line 2"
+            placeholderTextColor="gray"
+            onChangeText={value => list[6](value)}
+          />
+          <TextInput
+            style={styles.input_vm}
+            value={list[7]}
+            placeholder="Address Line 3"
+            placeholderTextColor="gray"
+            onChangeText={value => list[8](value)}
+          />
+          <Text style={styles.text_2}>
+            What is the full name of the Employee?
+          </Text>
+          <TextInput
+            style={styles.input_vm}
+            value={list[9]}
+            placeholder="Eg. John Smith Doe"
+            placeholderTextColor="gray"
+            onChangeText={value => list[10](value)}
+          />
+          <Text style={valid_email ? styles.hide : styles.text_warning}>
+            Please enter valid email
+          </Text>
+
+          <Text style={styles.text_2}>Description of Business:</Text>
+          <Text style={styles.text_2}>
+            What type of Business is the Company engaged in?
+          </Text>
 
           <TextInput
-            style={[styles.input, {marginHorizontal:0}]}
-            value={list[1].toLocaleDateString()}
+            style={styles.input_vm}
+            value={list[11]}
+            placeholder="Enter here"
+            placeholderTextColor="gray"
+            onChangeText={value => list[12](value)}
+          />
+
+          <Text style={styles.text_2}>Term</Text>
+          <Text style={styles.text_2}>
+            When is the term commencement (Start Date)?
+          </Text>
+          <TextInput
+            style={[styles.input, {marginHorizontal: 0}]}
+            value={list[13].toLocaleDateString()}
             placeholder="dd/mm/yyyy"
             placeholderTextColor="gray"
           />
@@ -48,89 +106,40 @@ const Policy1 = ({list}) => {
               </View>
             }
             color="#489503"
-            onSelect={value => list[2](value)}
+            onSelect={value => list[14](value)}
             isHideOnSelect={true}
             initialDate={new Date()}
           />
-
-          <Text style={{paddingVertical: 7}}>Clause 3</Text>
+          <Text style={styles.text_2}>Applicable Law</Text>
           <Text style={styles.text_2}>
-            Enter your Full Legal Name of the Party
+            Which State is the Company situated in?
           </Text>
           <TextInput
             style={styles.input_vm}
-            value={list[3]}
-            placeholder="Eg. John Smith Doe"
+            value={list[15]}
+            placeholder="Enter state name"
             placeholderTextColor="gray"
-            onChangeText={value => list[4](value)}
+            onChangeText={value => list[16](value)}
           />
-          <Text style={styles.text_2}>Website URL:</Text>
-          <TextInput
-            style={styles.input_vm}
-            value={list[5]}
-            placeholder="Enter here"
-            placeholderTextColor="gray"
-            onChangeText={value => {
-              if (value === '') {
-                setValid_url(true);
-              } else {
-                url_validation(value)
-                  ? setValid_url(true)
-                  : setValid_url(false);
-              }
-              list[6](value);
-            }}
-          />
-          <Text style={valid_url ? styles.hide : styles.text_warning}>
-            Please enter valid website url
-          </Text>
-          <Text style={{paddingVertical: 7}}>Clause 4.2b</Text>
-          <Text style={styles.text_2}>Enter your Email ID:</Text>
-          <TextInput
-            style={styles.input_vm}
-            value={list[7]}
-            placeholder="johndoe@gmail.com"
-            placeholderTextColor="gray"
-            onChangeText={value => {
-              if (value === '') {
-                setValid_email(true);
-              } else {
-                email_validation(value)
-                  ? setValid_email(true)
-                  : setValid_email(false);
-              }
-              list[8](value);
-            }}
-          />
-          <Text style={valid_email ? styles.hide : styles.text_warning}>
-            Please enter valid email
-          </Text>
-
-          <Text style={styles.text_2}>Written Permission:</Text>
-          <Text style={{paddingVertical: 7}}>Clause 4.4</Text>
           <Text style={styles.text_2}>
-            Enter your Email ID for acquiring Written Permission:
+            Which Country is the Company situated in?
           </Text>
-
-          <TextInput
-            style={styles.input_vm}
-            value={list[9]}
-            placeholder="johndoe@gmail.com"
-            placeholderTextColor="gray"
-            onChangeText={value => {
-              if (value === '') {
-                setValid_email1(true);
-              } else {
-                email_validation(value)
-                  ? setValid_email1(true)
-                  : setValid_email1(false);
-              }
-              list[10](value);
-            }}
-          />
-          <Text style={valid_email1 ? styles.hide : styles.text_warning}>
-            Please enter valid email
-          </Text>
+          <View style={styles.input_vm}>
+            {list[17] === '' ? (
+              <CountryPicker
+                // countryCode={country}
+                withFilter
+                withFlag
+                withCountryNameButton={list[17]}
+                withCallingCode
+                onSelect={value => {
+                  list[18](value.name);
+                }}
+              />
+            ) : (
+              <Text style={[styles.text_1, {fontSize: 17}]}>{list[17]}</Text>
+            )}
+          </View>
         </View>
       </ScrollView>
     </>
--- src/screens/PolicyGenerator/EmploymentContract/Steps.js
+++ src/screens/PolicyGenerator/EmploymentContract/Steps.js
@@ -1,191 +1,327 @@
-import {React,useState} from 'react'
+import {React, useState} from 'react';
 import {ProgressSteps, ProgressStep} from 'react-native-progress-steps';
-import { View } from 'react-native';
+import {View} from 'react-native';
 import Header from '../../../components/Header';
 import Policy1 from './Policy1';
 import Policy2 from './Policy2';
 import Policy3 from './Policy3';
 import Policy4 from '../Cookies/Policy4';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 
-import { empty_validation,email_validation } from '../validations';
-const generate_date = (date)=>{
-  const temp = date.split("/")
-  return "20"+temp[2]+"-"+temp[0]+"-"+temp[1]
- }
+import {empty_validation, email_validation} from '../validations';
+const generate_date = date => {
+  const temp = date.split('/');
+  return '20' + temp[2] + '-' + temp[0] + '-' + temp[1];
+};
 const Steps = () => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
-    const nextButton = {
-        backgroundColor: '#489503',
-        paddingHorizontal: 5,
-        borderRadius: 15,
-        width: 100,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 7,
-        elevation: 3,
-        alignSelf: 'center',
-      };
-    
-      const previousButton = {
-        backgroundColor: 'white',
-        paddingHorizontal: 5,
-        borderColor: '#489503',
-        borderWidth: 2,
-        borderRadius: 15,
-        width: 100,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 5,
-        alignSelf: 'center',
-      };
-      ///////////////////////////////////////////////////////////////////////////1
-      const [date, setDate] = useState(new Date());
-      const handle_date = (state)=> setDate(state);
-      const [input_1, setInput_1] = useState("");
-      const handle_input_1 = (state)=> setInput_1(state);
-      const [input_2, setInput_2] = useState("");
-      const handle_input_2 = (state)=> setInput_2(state);
-      const [input_3, setInput_3] = useState("");
-      const handle_input_3 = (state)=> setInput_3(state);
-      const [input_4, setInput_4] = useState("");
-      const handle_input_4 = (state)=> setInput_4(state);
-      const [empty_validationn, setempty_validation] = useState(true);
-      const states= [empty_validationn,date,handle_date,input_1,handle_input_1,input_2,handle_input_2,input_3,handle_input_3,input_4,handle_input_4,]
-      const [error_1, setError_1] = useState(false);
-      const inputs= [input_1,input_2,input_3,input_4]
-      ////////////////////////////////////////////////////////////////////////////2
-      const [empty_validationn_1, setempty_validation_1] = useState(true);
-      const [input_1_1, setInput_1_1] = useState('');
-      const handle_input_1_1 = (state)=> setInput_1_1(state);
-      const [input_2_1, setInput_2_1] = useState('EUR');
-      const handle_input_2_1 = (state)=> setInput_2_1(state);
-      const [input_3_1, setInput_3_1] = useState('');
-      const handle_input_3_1 = (state)=> setInput_3_1(state);
-      const [input_4_1, setInput_4_1] = useState('EUR');
-      const handle_input_4_1 = (state)=> setInput_4_1(state);
-      const [error_2, setError_2] = useState(false);
-      const [valid_number , setValid_number]=useState(true);
-      const handle_valid_number = (state)=> setValid_number(state);
-      const [valid_number1 , setValid_number1]=useState(true);
-      const handle_valid_number1 = (state)=> setValid_number1(state);
-      const states_1= [empty_validationn_1,input_1_1,handle_input_1_1,input_2_1,handle_input_2_1,input_3_1,handle_input_3_1,input_4_1,handle_input_4_1,valid_number,handle_valid_number,valid_number1,handle_valid_number1]
-      const inputs_1= [input_1_1,input_3_1]
-      /////////////////////////////////////////////////////////////////////////////3
-      const [input_1_2, setInput_1_2] = useState("");
-      const handle_input_1_2 = (state)=> setInput_1_2(state);
-      const [error_3, setError_3] = useState(false);
-      const [empty_validationn_2, setempty_validation_2] = useState(true);
-      const states_2= [input_1_2,handle_input_1_2,empty_validationn_2]
-      /////////////////////////////////////////////////////////////////////////////4
-      const [input_1_3, setInput_1_3] = useState("");
-      const handle_input_1_3 = (state)=> setInput_1_3(state);
-      const states_3= [input_1_3,handle_input_1_3,]
-      
-      const request_object={
-        agreement_compliance_type: "employment-contract",
-        last_update: generate_date(date.toLocaleDateString()),
-        party_full_name: input_1,
-        website_url: input_2,
-        email: input_3,
-        email_use_for_acquiring_written_permission: input_4,
-        liability_limit_amount: input_1_1,
-        liability_limit_amount_currency: input_2_1,
-        liability_must_not_exceed_amount: input_3_1,
-        liability_must_not_exceed_amount_currency: input_4_1,
-        email_for_requesting_access_to_personal_information:input_1_2
-      }
-    return (
-        <>
-          <Header title="Generator" />
-          <View
-            style={{
-              flex: 1,
-              paddingTop: 45,
-              backgroundColor: 'white',
-              paddingHorizontal:15,
+  const nextButton = {
+    backgroundColor: '#489503',
+    paddingHorizontal: 5,
+    borderRadius: 15,
+    width: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 7,
+    elevation: 3,
+    alignSelf: 'center',
+  };
+
+  const previousButton = {
+    backgroundColor: 'white',
+    paddingHorizontal: 5,
+    borderColor: '#489503',
+    borderWidth: 2,
+    borderRadius: 15,
+    width: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 5,
+    alignSelf: 'center',
+  };
+  ///////////////////////////////////////////////////////////////////////////1
+  const [empty_validationn, setempty_validation] = useState(true);
+  const [input_1, setInput_1] = useState('');
+  const handle_input_1 = state => setInput_1(state);
+  const [input_2, setInput_2] = useState('');
+  const handle_input_2 = state => setInput_2(state);
+  const [input_3, setInput_3] = useState('');
+  const handle_input_3 = state => setInput_3(state);
+  const [input_4, setInput_4] = useState('');
+  const handle_input_4 = state => setInput_4(state);
+  const [input_5, setInput_5] = useState('');
+  const handle_input_5 = state => setInput_5(state);
+  const [input_6, setInput_6] = useState('');
+  const handle_input_6 = state => setInput_6(state);
+  const [date, setDate] = useState(new Date());
+  const handle_date = state => setDate(state);
+  const [input_7, setInput_7] = useState('');
+  const handle_input_7 = state => setInput_7(state);
+  const [input_8, setInput_8] = useState('');
+  const handle_input_8 = state => setInput_8(state);
+  const states = [
+    empty_validationn,
+    input_1,
+    handle_input_1,
+    input_2,
+    handle_input_2,
+    input_3,
+    handle_input_3,
+    input_4,
+    handle_input_4,
+    input_5,
+    handle_input_5,
+    input_6,
+    handle_input_6,
+    date,
+    handle_date,
+    input_7,
+    handle_input_7,
+    input_8,
+    handle_input_8,
+  ];
+  const [error_1, setError_1] = useState(false);
+  const inputs = [
+    input_1,
+    input_2,
+    input_3,
+    input_4,
+    input_5,
+    input_6,
+    input_7,
+    input_8,
+  ];
+  ////////////////////////////////////////////////////////////////////////////2
+  const [radioButtons, setRadioButtons] = useState([
+    {
+      id: '1',
+      label: 'Hourly',
+      value: 'Hourly',
+      size: 18,
+      color: '#489503',
+      borderColor: '#585858',
+      selected: true,
+      labelStyle: {
+        color: '#585858',
+      },
+    },
+    {
+      id: '2',
+      label: 'Daily',
+      value: 'Daily',
+      size: 18,
+      color: '#489503',
+      borderColor: '##585858',
+      labelStyle: {
+        color: '#585858',
+      },
+    },
+    {
+      id: '3',
+      label: 'Weekly',
+      value: 'Weekly',
+      size: 18,
+      color: '#489503',
+      borderColor: '##585858',
+      labelStyle: {
+        color: '#585858',
+      },
+    },
+    {
+      id: '4',
+      label: 'Monthly',
+      value: 'Monthly',
+      size: 18,
+      color: '#489503',
+      borderColor: '##585858',
+      labelStyle: {
+        color: '#585858',
+      },
+    },
+  ]);
+
+  let a;
+  if (radioButtons[0].selected === true) {
+    a = 'Hourly';
+  } else if (radioButtons[1].selected === true) {
+    a = 'Daily';
+  } else if (radioButtons[1].selected === true) {
+    a = 'Weekly';
+  } else {
+    a = 'Monthly';
+  }
+
+  const [empty_validationn_1, setempty_validation_1] = useState(true);
+  const [input_1_1, setInput_1_1] = useState('');
+  const handle_input_1_1 = state => setInput_1_1(state);
+  const handle_radiobuttons = state => setRadioButtons(state);
+  const [input_3_1, setInput_3_1] = useState('');
+  const handle_input_3_1 = state => setInput_3_1(state);
+  const [input_4_1, setInput_4_1] = useState('EUR');
+  const handle_input_4_1 = state => setInput_4_1(state);
+  const [error_2, setError_2] = useState(false);
+  const [valid_number, setValid_number] = useState(true);
+  const handle_valid_number = state => setValid_number(state);
+  const states_1 = [
+    empty_validationn_1,
+    input_1_1,
+    handle_input_1_1,
+    radioButtons,
+    handle_radiobuttons,
+    input_3_1,
+    handle_input_3_1,
+    input_4_1,
+    handle_input_4_1,
+    valid_number,
+    handle_valid_number,
+  ];
+  const inputs_1 = [input_1_1, input_3_1];
+  /////////////////////////////////////////////////////////////////////////////3
+  const [empty_validationn_2, setempty_validation_2] = useState(true);
+  const [input_1_2, setInput_1_2] = useState('');
+  const handle_input_1_2 = state => setInput_1_2(state);
+  const [input_2_2, setInput_2_2] = useState('');
+  const handle_input_2_2 = state => setInput_2_2(state);
+  const [input_3_2, setInput_3_2] = useState('');
+  const handle_input_3_2 = state => setInput_3_2(state);
+  const [input_4_2, setInput_4_2] = useState('');
+  const handle_input_4_2 = state => setInput_4_2(state);
+  const [input_5_2, setInput_5_2] = useState('');
+  const handle_input_5_2 = state => setInput_5_2(state);
+  const [date1, setDate1] = useState(new Date());
+  const handle_date1 = state => setDate1(state);
+  const [input_6_2, setInput_6_2] = useState('');
+  const handle_input_6_2 = state => setInput_6_2(state);
+  const [input_7_2, setInput_7_2] = useState('');
+  const handle_input_7_2 = state => setInput_7_2(state);
+  const [date2, setDate2] = useState(new Date());
+  const handle_date2 = state => setDate2(state);
+  const [error_3, setError_3] = useState(false);
+  const states_2 = [
+    empty_validationn_2,
+    input_1_2,
+    handle_input_1_2,
+    input_2_2,
+    handle_input_2_2,
+    input_3_2,
+    handle_input_3_2,
+    input_4_2,
+    handle_input_4_2,
+    input_5_2,
+    handle_input_5_2,
+    date1,
+    handle_date1,
+    input_6_2,
+    handle_input_6_2,
+    input_7_2,
+    handle_input_7_2,
+    date2,
+    handle_date2,
+  ];
+  /////////////////////////////////////////////////////////////////////////////4
+  const [input_1_3, setInput_1_3] = useState('');
+  const handle_input_1_3 = state => setInput_1_3(state);
+  const states_3 = [input_1_3, handle_input_1_3];
+
+  const request_object = {
+    // agreement_compliance_type: 'employment-contract',
+    // last_update: generate_date(date.toLocaleDateString()),
+    // party_full_name: input_1,
+    // website_url: input_2,
+    // email: input_3,
+    // email_use_for_acquiring_written_permission: input_4,
+    // liability_limit_amount: input_1_1,
+    // liability_limit_amount_currency: input_2_1,
+    // liability_must_not_exceed_amount: input_3_1,
+    // liability_must_not_exceed_amount_currency: input_4_1,
+    // email_for_requesting_access_to_personal_information: input_1_2,
+  };
+  return (
+    <>
+      <Header title="Generator" />
+      <View
+        style={{
+          flex: 1,
+          paddingTop: 45,
+          backgroundColor: 'white',
+          paddingHorizontal: 15,
+        }}>
+        <ProgressSteps
+          style={{width: '100px'}}
+          marginBottom={15}
+          nextBtnTextStyle={{color: 'white', fontSize: 18}}>
+          <ProgressStep
+            nextBtnStyle={nextButton}
+            nextBtnTextStyle={{color: 'white', fontSize: 18}}
+            errors={error_1}
+            onNext={() => {
+              setempty_validation(empty_validation(inputs));
+              const x = empty_validation(inputs);
+              // const y= email_validation(input_3)
+              // const z= email_validation(input_4)
+              setError_1(!x);
             }}>
-            <ProgressSteps
-              style={{width: '100px'}}
-              marginBottom={15}
-              nextBtnTextStyle={{color: 'white', fontSize: 18}}>
-              <ProgressStep
-                nextBtnStyle={nextButton}
-                nextBtnTextStyle={{color: 'white', fontSize: 18}}
-                errors={error_1}
-                onNext={()=>{
-                  setempty_validation(empty_validation(inputs))
-                  const x=(empty_validation(inputs))
-                  const y= email_validation(input_3)
-                  const z= email_validation(input_4)
-                  setError_1(!(x & y & z) )
-                  
-                }}>
-                <View >
-                  <Policy1 list={states}/>
-                </View>
-              </ProgressStep>
-              <ProgressStep
-                nextBtnStyle={nextButton}
-                nextBtnTextStyle={{color: 'white', fontSize: 18}}
-                previousBtnTextStyle={{color: '#489503', fontSize: 18}}
-                previousBtnStyle={previousButton}
-                errors={error_2}
-                onNext={()=>{
-                  setempty_validation_1(empty_validation(inputs_1))
-                  const x= empty_validation(inputs_1)
-                  setError_2(!(x & valid_number & valid_number1) )
-                  
-                }}>
-                  
-                <View >
-                  <Policy2 list={states_1} />
-                </View>
-              </ProgressStep>
-              <ProgressStep
-                nextBtnStyle={nextButton}
-                nextBtnTextStyle={{color: 'white', fontSize: 18}}
-                previousBtnTextStyle={{color: '#489503', fontSize: 18}}
-                previousBtnStyle={previousButton}
-                errors={error_3}
-                onNext={()=>{
-                  setempty_validation_2(empty_validation([input_1_2]))
-                  const x= empty_validation([input_1_2])
-                  const y= email_validation(input_1_2)
-                  setError_3(!(x & y) )
-                }}
-                >
-                <View >
-                  <Policy3 list={states_2} />
-                </View>
-              </ProgressStep>
-              <ProgressStep
-                nextBtnStyle={nextButton}
-                nextBtnTextStyle={{color: 'white', fontSize: 18}}
-                previousBtnTextStyle={{color: '#489503', fontSize: 18}}
-                finishBtnText="Done"
-                previousBtnStyle={previousButton}
-                onSubmit={()=>{
-                  navigation.navigate('HomeScreen')
-                  // const y= email_validation(input_1_3)
-                  // const z= ( !y)
-                  // if (z){
-                  //   alert("please enter valid email")
-                  // }
-                  // else{
-                  //   //api
-                  // }
-                  
-                }}>
-                <View >
-                  <Policy4 list={states_3} object={request_object} />
-                </View>
-              </ProgressStep>
-            </ProgressSteps>
-          </View>
-        </>
-      );
-}
+            <View>
+              <Policy1 list={states} />
+            </View>
+          </ProgressStep>
+          <ProgressStep
+            nextBtnStyle={nextButton}
+            nextBtnTextStyle={{color: 'white', fontSize: 18}}
+            previousBtnTextStyle={{color: '#489503', fontSize: 18}}
+            previousBtnStyle={previousButton}
+            errors={error_2}
+            onNext={() => {
+              setempty_validation_1(empty_validation(inputs_1));
+              const x = empty_validation(inputs_1);
+              setError_2(!(x & valid_number));
+            }}>
+            <View>
+              <Policy2 list={states_1} />
+            </View>
+          </ProgressStep>
+          <ProgressStep
+            nextBtnStyle={nextButton}
+            nextBtnTextStyle={{color: 'white', fontSize: 18}}
+            previousBtnTextStyle={{color: '#489503', fontSize: 18}}
+            previousBtnStyle={previousButton}
+            errors={error_3}
+            onNext={() => {
+              setempty_validation_2(empty_validation([input_1_2]));
+              const x = empty_validation([input_1_2]);
+              const y = email_validation(input_1_2);
+              setError_3(!(x & y));
+            }}>
+            <View>
+              <Policy3 list={states_2} />
+            </View>
+          </ProgressStep>
+          <ProgressStep
+            nextBtnStyle={nextButton}
+            nextBtnTextStyle={{color: 'white', fontSize: 18}}
+            previousBtnTextStyle={{color: '#489503', fontSize: 18}}
+            finishBtnText="Done"
+            previousBtnStyle={previousButton}
+            onSubmit={() => {
+              navigation.navigate('HomeScreen');
+              // const y= email_validation(input_1_3)
+              // const z= ( !y)
+              // if (z){
+              //   alert("please enter valid email")
+              // }
+              // else{
+              //   //api
+              // }
+            }}>
+            <View>
+              <Policy4 list={states_3} object={request_object} />
+            </View>
+          </ProgressStep>
+        </ProgressSteps>
+      </View>
+    </>
+  );
+};
 
-export default Steps
+export default Steps;
