3c33420da1a7dbff820a50768bffe8f72c97a427
omair <omairwaleed17gmail.com>
2023-03-23 17:39:29 +0200

    changes in eula after testing

--- src/screens/PolicyGenerator/EULA/Policy2.js
+++ src/screens/PolicyGenerator/EULA/Policy2.js
@@ -5,9 +5,24 @@ import styles from '../Cookies/style'
 import RadioGroup from 'react-native-radio-buttons-group';
 import { SelectList } from 'react-native-dropdown-select-list'
 import { number_validation } from '../validations';
+import SelectDropdown from 'react-native-select-dropdown'
 const Policy2 = ({list}) => {
 
-
+const cuurency = [
+    'EUR',
+    'GBP',
+    'CAD',
+    'JPY',
+    'CHF',
+    'JPY'
+]
+const findcurrency=(cvalue)=>{
+  for (let index = 0; index < cuurency.length; index++) {
+    if(cuurency[index]===cvalue){
+      return index 
+    }
+  }
+}
 const cities = [
     {key:'1', value:'Mumbai'},
     {key:'2', value:'Kolkata'},
@@ -42,24 +57,36 @@ return (
             onChangeText={(value)=>list[3](value)}
       /> 
       <Text style={styles.text_3}>Remedies Under Limitation Of Liability</Text> 
-      <Text style={styles.text_2}>Which state laws will apply for this EULA?</Text>
-      <TextInput
-            style={styles.input_vm}
-            value={list[4]}
-            placeholder="  Enter Amount(₹)"
-            keyboardType='numeric'
-            placeholderTextColor="gray"
-            onChangeText={(value)=>{
-              if(value===""){
-                list[14](true)
-              }
-              else{
-                number_validation(value)?list[14](true):list[14](false)
-              }
-              list[5](value)
-              
-          }}
-          />
+      <Text style={styles.text_2}>What should be the liability remedy amount?</Text>
+      <View style={{display:"flex",flexDirection:"row"}}>
+        <TextInput
+              style={styles.input_vm_w}
+              value={list[4]}
+              placeholder="  Enter Amount(₹)"
+              keyboardType='numeric'
+              placeholderTextColor="gray"
+              onChangeText={(value)=>{
+                if(value===""){
+                  list[14](true)
+                }
+                else{
+                  number_validation(value)?list[14](true):list[14](false)
+                }
+                list[5](value)
+                
+            }}
+            />
+        <SelectDropdown
+                data={cuurency}
+                onSelect={(selectedItem, index) => {
+                  list[16](selectedItem)
+                }}
+                buttonStyle={{backgroundColor:"#D9D9D9",marginVertical:12,color:"#585858",borderWidth: 1,borderRadius: 15,borderColor: '#C4C4C4',height: 51,width:"25%"}}
+                buttonTextStyle={{color:"#585858",fontSize:16,fontWeight:"300"}}
+                dropdownStyle={{borderRadius: 15}}
+                defaultValueByIndex={findcurrency(list[15])}
+        />
+      </View>
       <Text  style={list[13] ? styles.hide: styles.text_warning}>Please Enter valid number</Text>
       <Text style={styles.text_3}>Applicable Law</Text>
       <Text style={styles.text_2}>Which state laws will apply for this EULA?</Text>
