25078737da07960d8866c1f20442e5150ad0aa6d
omair <omairwaleed17gmail.com>
2023-05-25 00:02:06 +0300

    completed all questions and answers for license compatibilty in help chat ,also added functionility of asking more than 1 question

--- src/screens/HomeScreen/Help.js
+++ src/screens/HomeScreen/Help.js
@@ -34,6 +34,21 @@ const Help = ({showHelp,helpHanlderClose}) => {
     set_license_compatibility(question)
   }
 
+  const [moreq,set_moreq]=useState("")
+  const moreq_handler=(state)=>{
+    set_moreq(state)
+  }
+  useEffect(()=>{
+    if(moreq ==="Yes"){
+      setquery("")
+      set_license_compatibility("")
+      set_moreq("")
+    }
+    else if (moreq ==="No"){
+      helpHanlderClose()
+      set_moreq("")
+    }
+  },[moreq])
 
   const make_room = async() => {
     
@@ -70,44 +85,122 @@ const Help = ({showHelp,helpHanlderClose}) => {
                     <Text style={{fontSize:25 ,fontFamily:"Roboto",fontWeight:"500" ,color:"#FFFFFF",marginLeft:"55%"}}>x</Text>
                 </TouchableOpacity>
             </View>
-            <View style={{backgroundColor:"white",height:"93%",borderTopLeftRadius: 30,borderTopRightRadius:30,paddingVertical:20,paddingHorizontal:5}}>
-              <ScrollView >
-                <View style={{display:"flex",flexDirection:"row"}}>
-                  <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
-                  <View>
-                    <Message Message="Hi, we're here to help you." customer_app ="app"/>
-                    <Message Message="please select your preferred language." customer_app ="app"/>
-                  </View>
-                </View>
-                {language==="" && <LanguageSelect language_handler={language_handler}/>}
-                {language!=="" && <Message Message={language} customer_app ="customer" top={103}/>}
-                {(language!=="" ) &&  (
-                <View style={{display:"flex",flexDirection:"row",marginTop:55}}>
-                  <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
-                  <View>
-                    <Message Message="Select your query." customer_app ="app"/>
-                  </View>
-                </View>
-                )}
-                {(query ==="" && language!=="") && <Queryselect handler={query_handler} items={["Agreement Compliance","License Compatibility","Software License"]}/>}
-                {query!=="" && <Message Message={query} customer_app ="customer" top={211}/>}
-                {query === "License Compatibility" && (
-                <>
-                  <View style={{display:"flex",flexDirection:"row",marginTop:74}}>
-                    <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
-                    <Message Message="identify your query from these options." customer_app ="app"/>
-                   
-                  </View>
-                  
-                  {license_compatibility==="" &&<Queryselect handler={license_compatibility_handler} items={["What is License Compatibility?","How to check the compatibility of two licenses?","What is recommendation percentage?","How much accurate is recommendation percentage?","Do I need to pay to check license compatibility","Can I compare any two licenses?"]}/>}
-                 
-                  {license_compatibility!=="" &&<Message Message={license_compatibility} customer_app ="customer" top={300} />}
-                </>
-                )}
+            <View style={{backgroundColor:"white",flex:1,borderTopLeftRadius: 30,borderTopRightRadius:30,paddingVertical:20,paddingHorizontal:5}}>
+              <View style={{height:"90%"}}>
+                <ScrollView  >
+                  <View style={{minHeight:500}}>
+                    <View style={{display:"flex",flexDirection:"row"}}>
+                      <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
+                      <View>
+                        <Message Message="Hi, we're here to help you." customer_app ="app"/>
+                        <Message Message="please select your preferred language." customer_app ="app"/>
+                      </View>
+                    </View>
+                    {language==="" && <LanguageSelect language_handler={language_handler}/>}
+                    {language!=="" && <View style={{alignSelf:'flex-end'}}><Message Message={language} customer_app ="customer" /></View>}
+                    {(language!=="" ) &&  (
+                    <View style={{display:"flex",flexDirection:"row"}}>
+                      <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
+                      <View>
+                        <Message Message="Select your query." customer_app ="app"/>
+                      </View>
+                    </View>
+                    )}
+                    {(query ==="" && language!=="") && <Queryselect handler={query_handler} items={["Agreement Compliance","License Compatibility","Software License"]}/>}
+                    {query!=="" && <View style={{alignSelf:'flex-end'}}><Message Message={query} customer_app ="customer" /></View>}
+                    {query === "License Compatibility" && (
+                    <>
+                      <View style={{display:"flex",flexDirection:"row"}}>
+                        <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
+                        <Message Message="identify your query from these options." customer_app ="app"/>
+                      
+                      </View>
+                      
+                      {license_compatibility==="" &&<Queryselect handler={license_compatibility_handler} items={["What is License Compatibility?","How to check the compatibility of two licenses?","What is recommendation percentage?","How much accurate is recommendation percentage?","Do I need to pay to check license compatibility?","Can I compare any two licenses?","Would I be able to know why licenses are compatible or not?","Can I use this Legalzard for my organization or company?"]}/>}
+                      {license_compatibility!=="" &&<View style={{alignSelf:'flex-end'}}><Message Message={license_compatibility} customer_app ="customer" top={license_compatibility.length} /></View>}
+                      {license_compatibility==="What is License Compatibility?" && (
+                        <View style={{display:"flex",flexDirection:"row"}}>
+                          <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
+                          <View>
+                            <Message Message="License Compatibility is a compatibility test between two licenses." customer_app ="app"/>
+                            <Message Message="It gives you a test result where it shows whether you can use those two compared licenses in your application." customer_app ="app"/>
+                          </View>
+                        </View>
+                      )}
+
+                      {license_compatibility==="How to check the compatibility of two licenses?" && (
+                        <View style={{display:"flex",flexDirection:"row"}}>
+                          <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
+                          <View>
+                            <Message Message="You can find a tutorial on how to check compatibility on the compatibility test page" customer_app ="app"/>
+                            <Message Message="We have described it in step by step manner" customer_app ="app"/>
+                          </View>
+                        </View>
+                      )}
+
+                      {license_compatibility==="What is recommendation percentage?" && (
+                        <View style={{display:"flex",flexDirection:"row"}}>
+                          <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
+                          <View>
+                            <Message Message="Recommendation percentage is a approximate number of how much compatible are those licenses" customer_app ="app"/>
+                            <Message Message="Also it checks whether you can use them in your application" customer_app ="app"/>
+                          </View>
+                        </View>
+                      )}
+
+                      {license_compatibility==="How much accurate is recommendation percentage?" && (
+                        <View style={{display:"flex",flexDirection:"row"}}>
+                          <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
+                          <View>
+                            <Message Message="Recommendation percentage is calculated by using different parameters i.e. versions, limitations, permissions, conditions, warranty disclaimers, etc." customer_app ="app"/>
+                            <Message Message="So considering these factors recommendation percentage is pretty accurate" customer_app ="app"/>
+                          </View>
+                        </View>
+                      )}
+
+                      {license_compatibility==="Do I need to pay to check license compatibility?" && (
+                        <View style={{display:"flex",flexDirection:"row"}}>
+                          <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
+                          <View>
+                            <Message Message="No, license compatibility is totally free to use. You do not need to pay anything" customer_app ="app"/>
+                          </View>
+                        </View>
+                      )}
+
+                      {license_compatibility==="Would I be able to know why licenses are compatible or not?" && (
+                        <View style={{display:"flex",flexDirection:"row"}}>
+                          <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
+                          <View>
+                            <Message Message="Full in depth comparison is provided after the results from the check are displayed" customer_app ="app"/>
+                            <Message Message="Click on “read more” below the result to find out" customer_app ="app"/>
+                          </View>
+                        </View>
+                      )}
+
+                      {license_compatibility==="Can I use this Legalzard for my organization or company?" && (
+                        <View style={{display:"flex",flexDirection:"row"}}>
+                          <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
+                          <View>
+                            <Message Message="Yes. You can set up a personal account or an account for your organization" customer_app ="app"/>
+                          </View>
+                        </View>
+                      )}
 
 
-              </ScrollView>  
-              <View style={{display :"flex",flexDirection:"row",alignItems:"center",backgroundColor:"white",width:"100%",paddingVertical:12}}>
+                      {license_compatibility!=="" && (
+                        <>
+                        <View style={{display:"flex",flexDirection:"row"}}>
+                          <MaterialCommunityIcons name="android" size={25} backgroundColor="#078F04" color="#078F04" />
+                          <Message Message="DO you need more questions? " customer_app ="app"/>
+                        </View>
+                        <Queryselect handler={moreq_handler} items={["Yes", "No"]}/>
+                        </>)}
+                    </>
+                    )}
+                  </View>
+                </ScrollView>  
+              </View>
+              <View style={{display :"flex",flexDirection:"row",alignItems:"center",width:"100%",paddingVertical:12}}>
                   <TextInput
                       style={styles.input}
                   
